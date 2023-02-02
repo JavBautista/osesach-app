61d14ec1be6b1ff1f4186250beda94acf2c8431e
Wuitoonchi Tecnology <wuitoonchi@gmail.com>
2023-02-01 20:42:11 -0700

    fix loged user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private navController: NavController
+  ) {}
+
+  ionViewWillEnter(){
+   let loged = localStorage.getItem('token');
+   if(loged) { this.navController.navigateRoot(['tabs']); } else { this.navController.navigateRoot('login ')}
+  }
 }
